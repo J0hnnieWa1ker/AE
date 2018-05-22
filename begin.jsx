aaf748261d2ec5eaf9c8c1431460cc0deb58e962
JohnnieWa1ker <jon@walker.ninja>
2018-05-22 00:06:50 -0700

    clean up in vim

--- begin.jsx
+++ begin.jsx
@@ -1,1 +1,16 @@
-﻿{  // Creating Project  var currentProject  = (app.project) ? app.project: app.newProject();  // Composition Settings Object  var cs = { name: 'Begin',                  width: 3840,                   height: 2160,                   pixel: 1,                  time: 60,                  frame: 25                };    // Create Composition  var currentComposition = (currentProject.activeItem) ? currentProject.activeItem: currentProject.items.addComp(cs.name, cs.width, cs.height, cs.pixel, cs.time, cs.frame);  currentComposition.openInViewer();}
+﻿{
+// Creating Project
+var currentProject  = (app.project) ? app.project: app.newProject();
+// Composition Settings Object
+var cs = { name: 'Begin',
+	  width: 3840,
+	 height: 2160,
+	  pixel: 1,
+	   time: 60,
+	  frame: 25
+	};
+// Create Composition
+var currentComposition = (currentProject.activeItem) ? currentProject.activeItem: currentProject.items.addComp(cs.name, cs.width, cs.height, cs.pixel, cs.time, cs.frame);
+currentComposition.openInViewer();
+}
+
